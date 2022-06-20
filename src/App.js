4a766478bd635151f226a70f55bafcfd35c29ffb
jonesPCGAMER <jonescnoel@gmail.com>
2022-06-20 14:51:47 -0300

    ...

--- src/App.js
+++ src/App.js
@@ -3,7 +3,41 @@ import Footer from 'components/layouts/Footer/Footer';
 import Info from 'components/layouts/Info/Info';
 import Contents from 'components/layouts/Contents/Contents';
 
+import axios from 'axios';
+import React from 'react';
+
+const baseURL = 'https://api.github.com';
+
 function App() {
+  const [error, setError] = React.useState(null);
+
+  React.useEffect(() => {
+    // invalid url will trigger an 404 error
+    axios
+      .get(`${baseURL}/users/JonesCesarN/repos`)
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        setError(error);
+      });
+  }, []);
+
+  if (error)
+    return (
+      <h3
+        style={{
+          width: '100vw',
+          height: '100vh',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'red',
+        }}>
+        Error: {error.message}
+      </h3>
+    );
+
   return (
     <div className="wrapper">
       <Info />
